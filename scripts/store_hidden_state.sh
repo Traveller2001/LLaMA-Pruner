d7ed2c65c5d3e2954c8061e596543cc451d73b47
Traveller <ttraveller2001@gmail.com>
2024-10-04 20:33:16 +0800

    test store hidden state successfully

--- scripts/store_hidden_state.sh
+++ scripts/store_hidden_state.sh
@@ -1,5 +1,8 @@
-python store_hidden_states.py \
+python src/store_hidden_state.py \
     --model_name llama-2-7b \
     --model_path /141nfs/zhangqingyu_17/hf_models/llama-2-7b \
+    --dtype fp16 \
     --calib_set data/pg19.jsonl \
     --output_dir output \
+
+
