a43b28a15be90e6e401446f58181ffe87c40a8c7
Traveller <ttraveller2001@gmail.com>
2024-10-04 20:01:35 +0800

    Restructuring of data files

---
+++ scripts/store_hidden_state.sh
@@ -0,0 +1,5 @@
+python store_hidden_states.py \
+    --model_name llama-2-7b \
+    --model_path /141nfs/zhangqingyu_17/hf_models/llama-2-7b \
+    --calib_set data/pg19.jsonl \
+    --output_dir output \
