f6e33e8773090f1e067a785d9ea865fe60731c81
Traveller <972437823@qq.com>
2024-10-10 12:46:20 +0800

    review scripts

--- scripts/hook_attn_map.sh
+++ scripts/hook_attn_map.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
 model_name=Llama-3-8B
 python src/hook_attn_map.py \
- -m /141nfs/zhangqingyu_17/hf_models/llama-2-7b \
- -o /141nfs/zhangqingyu_17/code/LLaMA-Pruner/result/attn_map/$model_name/last_row \
+ -m //hf_models/llama-2-7b \
+ -o //LLaMA-Pruner/result/attn_map/$model_name/last_row \
